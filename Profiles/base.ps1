d91b274d45538a0517d225bff4ca28a53bda8306
Rumit Parakhiya <rumit.parakhiya@tarktech.com>
2023-02-01 18:04:05 +0530

    Add code for unnecessary applications remove from the device

--- Profiles/base.ps1
+++ Profiles/base.ps1
@@ -5,3 +5,65 @@ choco install firefox -y
 choco install googlechrome -y 
 choco install sharex -y 
 choco install microsoft-teams.install -y 
+
+# Cleanup applications
+function removeApp {
+    Param ([string]$appName)
+    Write-Output "Trying to remove $appName"
+    Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
+    Get-AppXProvisionedPackage -Online | Where DisplayNam -like $appName | Remove-AppxProvisionedPackage -Online
+}
+
+$applicationList = @(
+    "Microsoft.3DBuilder"
+    "Microsoft.BingFinance"
+    "Microsoft.BingFinance"
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingWeather"
+    "Microsoft.CommsPhone"
+    "Microsoft.FreshPaint"
+    "Microsoft.GetHelp"
+    "Microsoft.Getstarted"
+    "Microsoft.Messaging"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.Office.Sway"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.NetworkSpeedTest"
+    "Microsoft.Office.Sway"
+    "Microsoft.OneConnect"
+    "Microsoft.Print3D"
+    "Microsoft.SkypeApp"
+    "Microsoft.WindowsAlarms"
+    "Microsoft.WindowsFeedbackHub"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsPhone"
+    "Microsoft.WindowsSoundRecorder"
+    "Microsoft.XboxApp"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    "*Autodesk*"
+    "*BubbleWitch*"
+    "Disney*"
+    "king.com*"
+    "G5*"
+    "*Dell*"
+    "*Facebook*"
+    "*Keeper*"
+    "MarchofEmpires"
+    "McAfee"
+    "*Minecraft*"
+    "*Netflix*"
+    "*Twitter*"
+    "*Plex*"
+    "*Solitaire*"
+    "*.Duolingo-LearnLanguagesforFree"
+    "*.EclipseManager"
+    "ActiproSoftwareLLC.562882FEEB491" # Code Writer
+    "*.AdobePhotoshopExpress"
+);
+
+foreach ($app in $applicationList) {
+    removeApp $app
+}
