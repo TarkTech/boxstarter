fd19f0a84d84f8a28e518189998a3051d8f85ab3
Rumit Parakhiya <rumit.parakhiya@tarktech.com>
2023-02-03 12:09:57 +0530

    Remove backend profile

--- Profiles/backend.ps1
+++
@@ -1,6 +0,0 @@
-. .\Profiles\dev.ps1
-
-choco install visualstudio2022community -y 
-choco install sql-server-express -y
-choco install sql-server-management-studio -y 
-choco install dotnet-6.0-sdk -y 
